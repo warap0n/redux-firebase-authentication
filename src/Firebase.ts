80c30960316ce66d1259a6a04e1242bccb75a8c2
warap0n <iiryonpai@gmail.com>
2023-12-09 22:19:45 +0900

    .env added

--- src/Firebase.ts
+++ src/Firebase.ts
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // TODO: Replace the following with your app's Firebase project configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAsU-qyAxOZDUftDm2e90EEVw-U3cyM2qE",
-  authDomain: "auth-redux-7f767.firebaseapp.com",
-  projectId: "auth-redux-7f767",
-  storageBucket: "auth-redux-7f767.appspot.com",
-  messagingSenderId: "408377953530",
-  appId: "1:408377953530:web:1706940a626baabff6cd43",
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
 };
 
 const app = initializeApp(firebaseConfig);
